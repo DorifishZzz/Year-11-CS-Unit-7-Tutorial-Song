55ad6fb0bf3586624eaaf23f0734b69a43d43800
dorazhao <doraz2025@student.cis.edu.hk>
2023-03-10 08:48:16 +0800

    added song

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+
+
 public class Song {
 
-    private String
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+    private String title;
+
+    private String artist;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    private ArrayList<String>Listener = new ArrayList<>();
+
+    public int howMany(ArrayList<String>Name){
+        int newListener = 0;
+        int i;
+        for(String n : Name){
+            if(Listener.contains(n.toLowerCase())){
+                continue;
+            }
+            else{
+                Listener.add(n.toLowerCase());
+                newListener+=1;
+            }
+
+        }
+        return newListener;
+
+    }
+
+
 }
